2a3b0338d8d381f11a36ed3bd9c23699c6bb7c34
Katetei <kate.savik@gmail.com>
2022-11-12 13:47:10 +0300

    comm

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,10 @@
-//here will by JavaScript code ...
+$(document).ready(function(){
+    $('.main-slider').slick();
+  });
+
+
+  $('.review').slick({
+    infinite: true,
+    slidesToShow: 5.5,
+    slidesToScroll: 1
+  });
