aba0af7014302a65edb0c18e01c7857f66392a12
Katetei <kate.savik@gmail.com>
2022-11-12 14:09:10 +0300

    сщььше

--- src/js/main.js
+++ src/js/main.js
@@ -8,3 +8,10 @@ $(document).ready(function(){
     slidesToShow: 5.5,
     slidesToScroll: 1
   });
+
+//for mob ??
+  $('.review-mob').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1
+  });
