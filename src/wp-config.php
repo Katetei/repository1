39b76b58ebd65cbf6decb8a5c16c2b9312a102eb
Katetei <kate.savik@gmail.com>
2023-01-30 13:40:00 +0300

    commit

---
+++ src/wp-config.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./main.scss">
+    <title>Ballons</title>
+    <script src="./js/main.js" defer></script>
+  </head>
+  <body>
+    <div class="container">
+      <div class="header">
+          <img class="header__logo" src="./img/Icon-logo.svg" alt="logo">
+          <div class="header__title">
+            <h1 class="title-first">
+              <span>туры на воздушном шаре</span>
+              <span>незабываемые приключения ждут вас</span>
+            </h1>
+            <button class="btn_white">наш каталог</button>
+          </div>
+      </div>
+    </div>
+  </body>
+</html>
